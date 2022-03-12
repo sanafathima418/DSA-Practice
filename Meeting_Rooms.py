fe42566290883c73fbdce1f78c0db1da1a4597cf
Sana Fathima <sana.fathima@colorado.edu>
2022-03-12 16:22:40 -0700

    M1

---
+++ Meeting_Rooms.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/meeting-rooms
+
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        min_rooms = 1
+        
+        # Time Complexity O(nlogn)
+        # Space Complexity O(1)
+        
+        # 1. Sort the array based on end time
+        new_intervals = sorted(intervals,key=lambda x: x[1])
+        
+        # For every interval:
+        for i in range(1,len(new_intervals)):
+            # Check if curr interval start time is less than prev interval end time
+            if new_intervals[i][0] < new_intervals[i-1][1]:
+                # Return False
+                return False
+                
+        return True
+                
+        
